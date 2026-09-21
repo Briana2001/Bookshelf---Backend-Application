34f6207d85094e72ecccdfd01095a23a30cc3a51
ultimatefaloe <ultimatefaloe@outlool.com>
2026-09-21 16:58:29 +0100

    shelf view updated

--- shelf/urls.py
+++ shelf/urls.py
@@ -5,5 +5,9 @@ from . import views
 app_name = 'shelf'
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('books/', views.books, name='books'),
+    path('book/add/', views.book_add, name='book_add'),
+    path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('book/<int:pk>/edit/', views.book_edit, name='book_edit'),
+    path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
 ]
--- shelf/views.py
+++ shelf/views.py
@@ -2,4 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-  return HttpResponse("This is the home page of the shelf app.")
+  return render(request, 'shelf/home.html')
+
+def book_add(request):
+  return render(request, 'shelf/book_add.html')
+ 
+def books(request):
+  return render(request, 'shelf/books.html')
+
+def book_detail(request, pk):
+  return render(request, 'shelf/book_detail.html')
+
+def book_edit(request, pk):
+  return render(request, 'shelf/book_edit.html')
+
+def book_delete(request, pk):
+  return render(request, 'shelf/book_delete.html')
